14c1604656733cd40bc5ec12b663c1436f35db1c
Kris <34949793+olmaq@users.noreply.github.com>
2018-01-27 22:59:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var url = 'https://api.icndb.com/jokes/random';
+var url = 'http://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
 button.addEventListener('click', function() {
 	getJoke();
